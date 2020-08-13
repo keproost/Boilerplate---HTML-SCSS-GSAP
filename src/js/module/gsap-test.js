39b8c46aa1b5374b699c43155d679c3a83d9bdd4
Azullian <kevin@azullian.com>
2020-08-13 19:58:59 +0200

    Remove experiment not rendering first commit

--- src/js/module/gsap-test.js
+++ src/js/module/gsap-test.js
@@ -52,56 +52,6 @@ fadeOutOnLeaveOnEnter.forEach((fadeOutOnLeaveOnEnterElement, i) => {
     });
 });
 
-//title
-var tl = new TimelineLite, 
-    mySplitText = new SplitText("#text_1", {type:"words,chars"}), 
-    chars = mySplitText.chars;
-
-TweenLite.set("#text_1", {perspective:400});
-
-tl.staggerFrom(chars, 3, {opacity:0, scale:0, y:80, rotationX:90, transformOrigin:"0% 50% -50",  ease:Back.easeOut}, 0.02, "+=0");
-//adding glow color to title
-TweenMax.to(text_1, 0.1, {
-    textShadow:"5px 2px 15px rgba(145, 233, 0, 1)",             
-    color:"#2EFEF7"
-});
-
-
-//Split by Char
-var tl2 = new TimelineLite, 
-    mySplitText = new SplitText("#text_2", {type:"chars, words"}), 
-    chars = mySplitText.chars;
-
-TweenLite.set("#text_2", {perspective:400});
-
-tl2.staggerFrom(mySplitText.chars, 1, { delay:2, scale:4, autoAlpha:0,  rotationX:-180,  transformOrigin:"100% 50%", ease:Back.easeOut}, 0.05);
-
-
-// Split by Lines
-
-var tl3 = new TimelineLite, 
-    mySplitText = new SplitText("#text_3", {type:"lines"}), 
-    chars = mySplitText.chars;
-
-TweenLite.set("#text_3", {perspective:400});
-
-tl3.staggerFrom(mySplitText.lines, 0.5, {delay:4, opacity:0, rotationX:-180, transformOrigin:"top center -250"}, 0.5);
- 
-//Split mix
-
-var tl4 = new TimelineLite, 
-    mySplitText = new SplitText("#text_4", {type:"chars, words, lines"}), 
-    chars = mySplitText.chars;
-
-TweenLite.set("#text_4", {perspective:400});
-
-tl4.staggerFrom(mySplitText.chars, 0.2, {delay:5,autoAlpha:0, scale:4}, 0.01, 0.5)
-    .staggerTo(mySplitText.words, 0.1, {color:"#A9F5F2", scale:0.9}, 0.1, "words")
-    .staggerTo(mySplitText.words, 0.2, {color:"white", scale:1}, 0.1, "words+=0.1");
-
-TweenMax.to(text_4, 0.1, {
-    textShadow:"5px 2px 15px rgba(145, 233, 0, 1)",             
-    color:"#2EFEF7"});
 // const fadeOutOnLeave = gsap.utils.toArray('.gsap-fadeOutOnLeave');
 // fadeOutOnLeave.forEach((fadeOutOnLeaveElement, i) => {
 //     ScrollTrigger.create({
