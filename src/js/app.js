42381e2f6c4606dc2360180d9d61a8e2190b6c41
Azullian <kevin@azullian.com>
2020-08-18 20:05:25 +0200

    Parallax more tryouts

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,4 @@ import 'bootstrap';
 import './module/gsap-test/custom-onscroll-animations';
 // import './module/gsap-test/custom-onclick-animations';
 import './module/gsap-test/custom-pageload-animations';
+import './module/gsap-test/custom-parallax-animations';
--- src/js/module/gsap-test/custom-pageload-animations.js
+++ src/js/module/gsap-test/custom-pageload-animations.js
@@ -1,81 +1,11 @@
-import { gsap, ScrollTrigger } from 'gsap/all';
+import { gsap } from 'gsap/all';
 import barba from '@barba/core';
-import $ from "jquery";
-// import scrollAnimations from './custom-onscroll-animations';
-gsap.registerPlugin(ScrollTrigger);
-
-window.$ = $;
-window.jQuery = $;
+import scrollAnimations from './custom-onscroll-animations';
+import parallaxAnimations from './custom-parallax-animations';
 
 const pageTransitionDuration = 0.50;
 const pageTransitionStagger = 0.10;
 const pageTransitionEase = 'power2.inOut';
-
-//This function was in a sperate file but i get it called here
-function scrollAnimations() {
-  const fadeOutOnLeaveOnEnter = gsap.utils.toArray(".gsap-fadeOutOnLeave");
-  fadeOutOnLeaveOnEnter.forEach((fadeOutOnLeaveOnEnterElement, i) => {
-    const fadeOutOnLeaveOnEnterAnimation = gsap.timeline({});
-    fadeOutOnLeaveOnEnterAnimation
-      .from(fadeOutOnLeaveOnEnterElement, {
-        opacity: 1,
-        ease: "Back.easeInOut",
-      })
-      .to(fadeOutOnLeaveOnEnterElement, { opacity: 0 });
-
-    ScrollTrigger.create({
-      trigger: fadeOutOnLeaveOnEnterElement,
-      animation: fadeOutOnLeaveOnEnterAnimation,
-      start: "top 30%",
-      end: "bottom top",
-      scrub: 1,
-      once: false,
-    });
-  });
-
-  const slideInUpOnEnter = gsap.utils.toArray(".gsap-slideInUpOnEnter");
-  slideInUpOnEnter.forEach((slideInUpOnEnterElement, i) => {
-    const slideInUpOnEnterAnimation = gsap.timeline({});
-    slideInUpOnEnterAnimation
-      .from(slideInUpOnEnterElement, { y: 200, ease: "Back.easeOut" })
-      .to(slideInUpOnEnterElement, { y: 0 });
-
-    ScrollTrigger.create({
-      trigger: slideInUpOnEnterElement,
-      animation: slideInUpOnEnterAnimation,
-      start: "-100px bottom",
-      end: "50% 40%",
-      scrub: 1,
-      once: false,
-    });
-  });
-
-  const growInOnEnter = gsap.utils.toArray(".gsap-growInOnEnter");
-  growInOnEnter.forEach((growInOnEnterElement, i) => {
-    const growInOnEnterAnimation = gsap.timeline({});
-    growInOnEnterAnimation
-      .from(growInOnEnterElement, { scale: 0, ease: "Back.easeOut" })
-      .to(growInOnEnterElement, { scale: 1 });
-
-    ScrollTrigger.create({
-      trigger: growInOnEnterElement,
-      animation: growInOnEnterAnimation,
-      start: "-100px bottom",
-      end: "50% 40%",
-      scrub: 1,
-      once: false,
-    });
-  });
-}
-
-//Initial execution of scrollanimations when user lands directly on page with scroll animations
-scrollAnimations();
-
-//Execution of scrollanimations when user refreshes the page
-window.addEventListener('load', function(){
-  gsap.registerPlugin(ScrollTrigger);
-  ScrollTrigger.refresh();
-})
 
 
 // PAGE transitions using barba.js
@@ -133,8 +63,8 @@ barba.init({
       once: ({ next }) => enterAnimation(next.container.querySelector('main')),
       enter: ({ next }) => enterAnimation(next.container.querySelector('main')),
       afterEnter: () => {
-        console.log('index:afterEnter');
-        scrollAnimations(); //I need to call the function here so the triggers are recalculated after pagetransition ends
+        scrollAnimations();
+        parallaxAnimations();
       }
     }
   ]
@@ -182,40 +112,40 @@ barba.init({
 
 //     // reset position of the loading screen
 //     gsap.set(loader, {
-//         scaleX: 0, 
-//         rotation: 10, 
+//         scaleX: 0,
+//         rotation: 10,
 //         xPercent: -5,
-//         yPercent: -50, 
-//         transformOrigin: 'left center', 
+//         yPercent: -50,
+//         transformOrigin: 'left center',
 //         autoAlpha: 1
 //     });
 
 //     function loaderIn() {
 //         // GSAP tween to stretch the loading screen across the whole screen
-//         return gsap.fromTo(loader, 
+//         return gsap.fromTo(loader,
 //             {
 //                 rotation: 10,
 //                 scaleX: 0,
 //                 xPercent: -5
 //             },
-//             { 
+//             {
 //                 duration: 0.8,
 //                 xPercent: 0,
-//                 scaleX: 1, 
+//                 scaleX: 1,
 //                 rotation: 0,
-//                 ease: 'Power4.inOut', 
+//                 ease: 'Power4.inOut',
 //                 transformOrigin: 'left center'
 //             });
 //     }
 
 //     function loaderAway() {
 //         // GSAP tween to hide the loading screen
-//         return gsap.to(loader, { 
-//             duration: 0.8, 
+//         return gsap.to(loader, {
+//             duration: 0.8,
 //             scaleX: 0,
-//             xPercent: 5, 
-//             rotation: -10, 
-//             transformOrigin: 'right center', 
+//             xPercent: 5,
+//             rotation: -10,
+//             transformOrigin: 'right center',
 //             ease: 'Power4.inOut'
 //         });
 //     }
